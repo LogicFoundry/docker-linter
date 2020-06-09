3943e4d05164b8068891ef6fac4b6a9c881ec277
Damian Perera <damian.perera@sysco.com>
2020-06-09 19:52:36 +0530

    init: added editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Editor from 'react-simple-code-editor';
+// @ts-ignore
+import { highlight, languages } from 'prismjs/components/prism-core';
+import 'prismjs/themes/prism.css';
+import 'prismjs/components/prism-clike';
+import 'prismjs/components/prism-javascript';
 import './App.css';
 
+const exampleCode = `FROM ubuntu:18.04
+
+LABEL maintainer="damianperera@medium.com"
+
+RUN apt-get update -y && \\
+    apt-get install -y g++ build-essential gcc libc6-dev python2.7 python2.7-dev supervisor cron vim unzip curl wget tree
+
+COPY jdk-8u191-linux-x64.tar.gz .
+
+RUN mkdir -p /opt/jdk && \\
+    tar -zvxf jdk-8u191-linux-x64.tar.gz -C /opt/jdk && \\
+    rm jdk-8u191-linux-x64.tar.gz
+
+RUN update-alternatives --install /usr/bin/java java /opt/jdk/jdk1.8.0_191/bin/java 100 && \\
+    update-alternatives --install /usr/bin/javac javac /opt/jdk/jdk1.8.0_191/bin/javac 100 && \\
+    update-alternatives --display java && \\
+    update-alternatives --display javac && \\
+    java -version`
+
 function App() {
+  const [value, setValue] = useState(exampleCode);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="Editor">
+          <Editor
+            value={value}
+            onValueChange={setValue}
+            highlight={code => highlight(code, languages.js, 'docker')}
+            padding={10}
+            style={{
+              fontFamily: '"Fira code", "Fira Mono", monospace',
+              fontSize: 16,
+              width: 800
+            }}
+          />
+        </div>
       </header>
     </div>
   );
