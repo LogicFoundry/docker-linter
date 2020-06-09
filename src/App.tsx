56e50dc211944498d257e2e96741d08a81594c2f
Damian Perera <damian.perera@sysco.com>
2020-06-09 21:03:55 +0530

    init: added checkmark

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@ import React, { useState } from 'react';
 import Editor from 'react-simple-code-editor';
 // @ts-ignore
 import { highlight, languages } from 'prismjs/components/prism-core';
+// @ts-ignore
+import validator from 'validate-dockerfile';
+import checkmark from './checkmark.png';
 import 'prismjs/themes/prism.css';
 import 'prismjs/components/prism-clike';
 import 'prismjs/components/prism-javascript';
 import './App.css';
 
 const exampleCode = `FROM ubuntu:18.04
-
-LABEL maintainer="dev@logicfoundry.tech"
 
 RUN apt-get update -y && \\
     apt-get install -y g++ build-essential gcc libc6-dev python2.7 python2.7-dev supervisor cron vim unzip curl wget tree
@@ -24,10 +25,14 @@ RUN update-alternatives --install /usr/bin/java java /opt/jdk/jdk1.8.0_191/bin/j
     update-alternatives --install /usr/bin/javac javac /opt/jdk/jdk1.8.0_191/bin/javac 100 && \\
     update-alternatives --display java && \\
     update-alternatives --display javac && \\
-    java -version`
+    java -version
+
+CMD java -version`
 
 function App() {
   const [value, setValue] = useState(exampleCode);
+  const { valid, errors } = validator(value);
+  console.log(valid, errors)
   return (
     <div className="App">
       <header className="App-header">
@@ -35,7 +40,7 @@ function App() {
           <Editor
             value={value}
             onValueChange={setValue}
-            highlight={code => highlight(code, languages.js, 'docker')}
+            highlight={(code:string) => highlight(code, languages.js, 'docker')}
             padding={10}
             style={{
               fontFamily: '"Fira code", "Fira Mono", monospace',
@@ -43,6 +48,12 @@ function App() {
               width: 800
             }}
           />
+        </div>
+        <div className="Text">
+          <h4>{
+            valid && (<img alt="checkmark" src={checkmark} className="Checkmark"></img>)
+          }
+          </h4>
         </div>
       </header>
     </div>
