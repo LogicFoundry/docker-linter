4a8283d56ea38e029ddb4dea4934069cb8940e9e
damianperera <15967502+damianperera@users.noreply.github.com>
2020-06-10 05:03:30 +0000

    Deploying to gh-pages from  @ d18e1bc963d64774d847bda890859869238aa74a 🚀

---
+++ precache-manifest.6b4dba23cab8e0d88523f02314a12035.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dd0248c3e7bd0acd6c816f586830bde",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5aabb84d453762bc26a2",
+    "url": "/static/css/2.6ea54503.chunk.css"
+  },
+  {
+    "revision": "7bf370af3fb985ca77f8",
+    "url": "/static/css/main.f4a1aa1e.chunk.css"
+  },
+  {
+    "revision": "5aabb84d453762bc26a2",
+    "url": "/static/js/2.e25b9f20.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e25b9f20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bf370af3fb985ca77f8",
+    "url": "/static/js/main.543e0852.chunk.js"
+  },
+  {
+    "revision": "bb4b8bb62d308d9934a1",
+    "url": "/static/js/runtime-main.cf8f8b52.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6efdbb95bf4d738e05e103d706c52b5f.js"
+  "/precache-manifest.6b4dba23cab8e0d88523f02314a12035.js"
 );
 
 self.addEventListener('message', (event) => {
